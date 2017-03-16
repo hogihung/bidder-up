bd0dc4113cacad44ee2b401a858c3f95e380b316
John Hogarty <hogihung@gmail.com>
2017-03-15 21:07:48 -0400

    Add UiStr to implement using Constants to help improve performance.

---
+++ app/helpers/ui_str.rb
@@ -0,0 +1,11 @@
+module UiStr
+  PRODUCTID    = "ProductID"
+  TITLE        = "Title"
+  QUANTITY     = "Units"
+  HIGH_BID     = "High Bid Amount"
+  DEMAND       = "Demand"
+  RESERVE_MET  = "Reserve Met?"
+
+  BID_SUMMARY  = "Bid Summary"
+  BID_COUNT    = "BID COUNT: "
+end
