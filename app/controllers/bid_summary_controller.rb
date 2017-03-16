9c3eea32c640f2b75ff11d777c1c844c0c7e462a
John Hogarty <hogihung@gmail.com>
2017-03-16 13:08:01 -0400

    Update Ruby Prof call to output CG files to use new format per current version of Ruby Prof.

--- app/controllers/bid_summary_controller.rb
+++ app/controllers/bid_summary_controller.rb
@@ -13,11 +13,9 @@ require 'ruby-prof'
     render 'index'
     profile_result = RubyProf.stop                           # stops profiling, storing in variable
     RubyProf::FlatPrinter.new(profile_result).print(STDOUT)  # prints out to your server console
-    stampit = DateTime.now.strftime("%H_%M_%S")              # create a time stamp
 
     # Create a .cg file of the gathered data so we can view with qcachegrind
-    open("data/perf_profile/BEFORE/bid_summary_#{stampit}.cg", "w") do |f|
-      RubyProf::CallTreePrinter.new(profile_result).print(f)
-    end
+    stampit = DateTime.now.strftime("%H_%M_%S")              # create a time stamp
+    RubyProf::CallTreePrinter.new(profile_result).print(profile: "data/perf_profile/BEFORE/bid_summary-#{stampit}")
   end
 end
