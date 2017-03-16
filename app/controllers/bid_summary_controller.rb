9be0b3df37808a8ac4692faf344c93c5a6d45519
John Hogarty <hogihung@gmail.com>
2017-03-15 21:24:04 -0400

    Enable Ruby Prof to assist us with profiling the application.

--- app/controllers/bid_summary_controller.rb
+++ app/controllers/bid_summary_controller.rb
@@ -1,10 +1,23 @@
 class BidSummaryController < ApplicationController
+require 'ruby-prof'
 
   def index
+    RubyProf.start                                           # starts the profiling process
+
     products = Product.all
     bidders = Bidder.all
     bids = Bid.all
 
     @bid_summary = BidSummary::IndexView.new(products, bidders, bids)
+
+    render 'index'
+    profile_result = RubyProf.stop                           # stops profiling, storing in variable
+    RubyProf::FlatPrinter.new(profile_result).print(STDOUT)  # prints out to your server console
+    stampit = DateTime.now.strftime("%H_%M_%S")              # create a time stamp
+
+    # Create a .cg file of the gathered data so we can view with qcachegrind
+    open("data/perf_profile/BEFORE/bid_summary_#{stampit}.cg", "w") do |f|
+      RubyProf::CallTreePrinter.new(profile_result).print(f)
+    end
   end
 end
