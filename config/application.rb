20bde110f06b0d6e820a422efc27baddb50a51d6
John Hogarty <hogihung@gmail.com>
2017-03-16 13:06:46 -0400

    Add support for Ruby Prof - insert after Rack Middleware.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require_relative 'gc_disabler'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,5 +12,10 @@ module BidderUp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Setting to support Ruby Prof
+    config.middleware.use Rack::RubyProf, path: '/tmp/rails_profile'
+    config.middleware.insert_before Rack::RubyProf, GCDisabler
   end
 end
+
